aafb147a4be31d13c691b006485cc71c367b7bab
Juliana Phamova <phamovajuliana@gmail.com>
2021-12-01 21:04:19 +0100

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -193,12 +193,17 @@ void proccesDmaData(uint8_t sign)
 
         }
         if(mode == 1){
-            if(strncmp("$PWM",readed_text,4)==0){
-                for(int j = 0; j<3; j++){
-                    read_duty_cycle_text[j]=readed_text[j+4];
-                }
-                dutyCycleManual = atoi(read_duty_cycle_text);
+	    if (strlen(readed_text) != 6){//posledny $ sa nepridava do readed_text
                 strcpy(readed_text,"");
+	    }	    
+            if(strncmp("$PWM",readed_text,4)==0){
+		if((readed_text[4] > '0' && readed_text[4] < '9') && (readed_text[5] > '0' && readed_text[5] < '9')) {
+                    for(int j = 0; j<2; j++){
+                        read_duty_cycle_text[j]=readed_text[j+4];
+                    }
+                    dutyCycleManual = atoi(read_duty_cycle_text);
+                    strcpy(readed_text,"");
+		} else strcpy(readed_text,"");
             }
         }
     }
